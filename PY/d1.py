a91ce6fe8f3b4a78459e64e7a9b00826bc7ba1d2
Ece Karataş <ece.karatas@ozu.edu.tr>
2023-10-18 18:26:43 +0300

    date eklendi

--- PY/d1.py
+++ PY/d1.py
@@ -1,9 +1,15 @@
 import json
-import os
-import tempfile
 import tkinter as tk
 from tkinter import messagebox, ttk
+from tkcalendar import DateEntry
 from pydriller import Repository
+from datetime import datetime, timedelta, timezone
+
+# Define excluded extensions for various file types
+excluded_extensions = {
+    'compiled': ['.class', '.pyc'],
+    'system': ['.dll', '.exe', '.so']
+}
 
 def open_new_page(commit_count, author_names, changed_classes):
     new_page = tk.Toplevel(root)
@@ -18,7 +24,7 @@ def open_new_page(commit_count, author_names, changed_classes):
     frame = ttk.Frame(canvas)
     canvas.create_window((0, 0), window=frame, anchor=tk.NW)
 
-    for i, (label_text, data) in enumerate(zip(["Authors:", "Commits:", "Classes:"], [author_names, commit_count, changed_classes])):
+    for i, (label_text, data) in enumerate(zip(["Authors:", "Total Commits:", "Classes:"], [author_names, commit_count, changed_classes])):
         label = ttk.Label(frame, text=label_text)
         label.grid(row=0, column=i, padx=5, pady=5)
 
@@ -37,6 +43,15 @@ def open_new_page(commit_count, author_names, changed_classes):
     frame.update_idletasks()
     canvas.configure(scrollregion=canvas.bbox("all"), yscrollcommand=scrollbar.set)
 
+def toggle_date_entries():
+    # Enable or disable the DateEntry widgets based on the checkbox state
+    if date_checkbox_var.get():
+        date_entry1.configure(state="normal")
+        date_entry2.configure(state="normal")
+    else:
+        date_entry1.configure(state="disabled")
+        date_entry2.configure(state="disabled")
+
 def submit_button_clicked():
     github_link = entry.get()
 
@@ -45,14 +60,23 @@ def submit_button_clicked():
         messagebox.showinfo("Error", "Please enter a GitHub link.")
         return
 
-    # Create a temporary directory to store the commit data
-    temp_dir = tempfile.mkdtemp()
-    temp_file_path = os.path.join(temp_dir, 'commit_data.json')
+    # Check if the user wants to filter by date
+    if date_checkbox_var.get():
+        # Get the selected dates from the DateEntry widgets
+        if date_entry1.get_date() and date_entry2.get_date():
+            dt1 = datetime.combine(date_entry1.get_date(), datetime.min.time(), tzinfo=timezone.utc)
+            dt2 = datetime.combine(date_entry2.get_date(), datetime.max.time(), tzinfo=timezone.utc)
+        else:
+            messagebox.showinfo("Error", "Please select both start and end dates.")
+            return
+    else:
+        dt1 = None
+        dt2 = None
 
     try:
         # Write commit data to a temporary JSON file
         commit_data = []
-        for commit in Repository(github_link).traverse_commits():
+        for commit in Repository(github_link, since=dt1, to=dt2).traverse_commits():
             commit_info = {
                 'hash': commit.hash,
                 'message': commit.msg,
@@ -61,13 +85,14 @@ def submit_button_clicked():
             }
             commit_data.append(commit_info)
 
-        with open(temp_file_path, 'w') as outfile:
+        output_file_path = 'commit_data.json'
+        with open(output_file_path, 'w') as outfile:
             json.dump(commit_data, outfile, indent=4)
 
-        print('Commit data written to', temp_file_path)
+        print('Commit data written to', output_file_path)
 
         # Load commit data from the temporary JSON file
-        with open(temp_file_path, 'r') as infile:
+        with open(output_file_path, 'r') as infile:
             loaded_commit_data = json.load(infile)
 
         # Extract unique author names from the commit data
@@ -79,19 +104,16 @@ def submit_button_clicked():
             for modified_file in commit['modified_files']:
                 if modified_file.endswith('.java'):  # Assuming the files are Java classes
                     changed_classes.add(modified_file)
-
+                if modified_file.endswith('.py'):  # Assuming the files are Py classes
+                    changed_classes.add(modified_file)
+                if modified_file.endswith('.cpp'):  # Assuming the files are C++ classes
+                    changed_classes.add(modified_file)
+                    
         commit_count = len(loaded_commit_data)
         open_new_page(commit_count, author_names, changed_classes)
 
     except Exception as e:
         messagebox.showinfo("Error", f"An error occurred: {str(e)}")
-
-    finally:
-        # Clean up temporary files and directory
-        if os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
-        if os.path.exists(temp_dir):
-            os.rmdir(temp_dir)
 
 # Create the main application window
 root = tk.Tk()
@@ -100,6 +122,22 @@ root.title("GitHub Link Input")
 # Create and configure the widgets
 entry = tk.Entry(root, width=50)
 entry.pack(pady=5, padx=10)  # Adding padding to the left and right
+
+# Checkbox to enable/disable date selections
+date_checkbox_var = tk.BooleanVar()
+date_checkbox = ttk.Checkbutton(root, text="Enable Date Selection", variable=date_checkbox_var, command=toggle_date_entries)
+date_checkbox.pack(pady=5)
+
+# DateEntry widgets for selecting the time range
+date_label1 = tk.Label(root, text="Select start date:")
+date_label1.pack(pady=5)
+date_entry1 = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2, state="disabled")
+date_entry1.pack(pady=5, padx=(0, 10))  # Adding padding to the right
+
+date_label2 = tk.Label(root, text="Select end date:")
+date_label2.pack(pady=5)
+date_entry2 = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2, state="disabled")
+date_entry2.pack(pady=5)
 
 submit_button = tk.Button(root, text="Submit", command=submit_button_clicked)
 submit_button.pack(pady=10)
