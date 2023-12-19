98d6042fbdbda0fb73834847e6d1371971870cc4
tugceozgirgin <93055813+tugceozgirgin@users.noreply.github.com>
2023-12-19 19:05:23 +0300

    reachable files and jackness with graph algorithms

--- PY/analyzes/graph_algorithms.py
+++ PY/analyzes/graph_algorithms.py
@@ -21,11 +21,91 @@ class GraphAlgorithms:
             nodes = [record['n'] for record in result]
             return nodes
 
-    # Define other methods to perform specific operations on the database
+    def get_developers(self):
+        with self._driver.session() as session:
+            result = session.run("MATCH (d:Developer) RETURN d.developer_id AS developer_id")
+            developers = [record["developer_id"] for record in result]
+            return developers
+
+    def get_files_for_developer(self, developer_id):
+        with self._driver.session() as session:
+            result = session.run(
+                "MATCH (d:Developer {developer_id: $developer_id})-[:DEVELOPED_BY]->(c:Commit)-[:MODIFIED]->(f:Files) RETURN f",
+                developer_id=developer_id,
+            )
+            files = [record['f'] for record in result]
+            return files
+
+    def dfs(self, start_node, visited=None, reachable_files=None):
+        if visited is None:
+            visited = set()
+        if reachable_files is None:
+            reachable_files = set()
+
+        visited.add(start_node)
+        files = self.get_files_for_developer(start_node)
+        reachable_files.update(files)
+
+        adjacent_nodes = []
+
+        for node in adjacent_nodes:
+            if node not in visited:
+                self.dfs(node, visited, reachable_files)
+
+        return reachable_files
+
+    def dev_to_files(self):
+        developers = self.get_developers()
+        dev_to_reachable_files = {}
+
+        for dev_id in developers:
+            reachable_files = self.dfs(dev_id)
+            dev_to_reachable_files[dev_id] = reachable_files
+
+        return dev_to_reachable_files
+
+    def get_num_files(self):
+        all_files = set()
+        with self._driver.session() as session:
+                result = session.run("MATCH (f:Files) RETURN f.file_name")
+                all_files.update(record["f.file_name"] for record in result)
+
+        return len(all_files)
+    def find_jacks(self):
+        dev_to_files = self.dev_to_files()
+        dev_to_file_coverage = {}
+        num_files = self.get_num_files()
+
+        for dev, files in dev_to_files.items():
+            num_dev_files = len(files)
+            file_coverage = num_dev_files / num_files if num_files > 0 else 0
+            dev_to_file_coverage[dev] = file_coverage
+
+        sorted_dev_to_file_coverage = self.sort_by_value(dev_to_file_coverage)
+        return sorted_dev_to_file_coverage
+    @staticmethod
+    def sort_by_value(dictionary):
+        sorted_items = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+        return dict(sorted_items)
+
+    def is_jack(self, dev_to_file_coverage, developer):
+        treshold = 0.19
+        return dev_to_file_coverage.get(developer, 0) > treshold
 
 
-#grap instance creation pulling all the nodes
-graph_algo = GraphAlgorithms()
-nodes = graph_algo.get_all_nodes()
-print(nodes)
-graph_algo.close()
+
+# Graph instance creation and function execution
+graph = GraphAlgorithms()
+developers = graph.get_developers()
+print(developers)
+reachable_files = graph.dev_to_files()
+print(reachable_files)
+
+result = graph.find_jacks()
+# Print the result to the console
+print("Developer Classification:")
+for developer, file_coverage in result.items():
+    print(f"{developer}: {file_coverage:.2%} (Jack: {graph.is_jack(result, developer)})")
+graph.close()
+
+
